923a7ef1ed9139953075e5c72c91f8cce88eefa8
mawei <mawei@zhongwenweilai.com>
2018-10-12 15:41:09 +0800

    增加筛选

--- Part.js
+++ Part.js
@@ -1,11 +1,9 @@
-
 //from: 0:微信  1:支付宝 2:QQ  3:现金
 
 //type: 0:村里 1:唐县初中  2:石家庄铁路  3:保定软件学院 4: 倍乐维 
 
 //status: 0已送 1未送
-var PartJson = [
-    {
+var PartJson = [{
         id: 0,
         name: "王少辉",
         moneyfrom: 3,
@@ -71,363 +69,369 @@ var PartJson = [
         type: 0,
         status: 1,
         money: 200
-    },{
+    }, {
         id: 8,
         name: "于子萌",
         moneyfrom: 0,
         type: 0,
         status: 1,
         money: 200
-    },{
+    }, {
         id: 9,
         name: "于笑莹",
         moneyfrom: 0,
         type: 0,
         status: 1,
         money: 200
-    },{
+    }, {
         id: 10,
         name: "赵曼",
         moneyfrom: 0,
         type: 1,
         status: 1,
         money: 200
-    },{
+    }, {
         id: 11,
         name: "马海立",
         moneyfrom: 0,
         type: 0,
         status: 0,
         money: 500
-    },{
+    }, {
         id: 12,
         name: "马硕",
         moneyfrom: 0,
         type: 0,
         status: 1,
         money: 200
-    },{
+    }, {
         id: 13,
         name: "于丹洋",
         moneyfrom: 0,
-        type: 0,        //0:村里 1:唐县初中  2:石家庄铁路  3:保定软件学院 4: 倍乐维
-        status: 0,      // 0已送 1未送
+        type: 0, 
+        status: 0, 
         money: 200
-    },{
+    }, {
         id: 14,
         name: "马泽涛",
         moneyfrom: 0,
-        type: 0,        //0:村里 1:唐县初中  2:石家庄铁路  3:保定软件学院 4: 倍乐维
-        status: 1,      // 0已送 1未送
+        type: 0, 
+        status: 1, 
         money: 200
-    },{
+    }, {
         id: 15,
         name: "于子飞",
         moneyfrom: 0,
-        type: 0,        //0:村里 1:唐县初中  2:石家庄铁路  3:保定软件学院 4: 倍乐维
-        status: 1,      // 0已送 1未送
+        type: 0, 
+        status: 1, 
         money: 200
-    },{
+    }, {
         id: 16,
         name: "袁子波",
         moneyfrom: 0,
-        type: 0,        //0:村里 1:唐县初中  2:石家庄铁路  3:保定软件学院 4: 倍乐维
-        status: 1,      // 0已送 1未送
+        type: 0, 
+        status: 1, 
         money: 200
-    },{
+    }, {
         id: 17,
         name: "赵瑞",
         moneyfrom: 0,
-        type: 0,        //0:村里 1:唐县初中  2:石家庄铁路  3:保定软件学院 4: 倍乐维
-        status: 1,      // 0已送 1未送
+        type: 0, 
+        status: 1, 
         money: 200
-    },{
+    }, {
         id: 18,
         name: "陈少阳",
         moneyfrom: 0,
-        type: 2,        //0:村里 1:唐县初中  2:石家庄铁路  3:保定软件学院 4: 倍乐维
-        status: 1,      // 0已送 1未送
+        type: 2, 
+        status: 1, 
         money: 500
-    },{
+    }, {
         id: 19,
         name: "袁子怡",
         moneyfrom: 0,
-        type: 0,        //0:村里 1:唐县初中  2:石家庄铁路  3:保定软件学院 4: 倍乐维
-        status: 1,      // 0已送 1未送
+        type: 0, 
+        status: 1, 
         money: 200
-    },{
+    }, {
         id: 20,
         name: "刘志锐",
         moneyfrom: 0,
-        type: 3,        //0:村里 1:唐县初中  2:石家庄铁路  3:保定软件学院 4: 倍乐维
-        status: 1,      // 0已送 1未送
+        type: 3, 
+        status: 1, 
         money: 500
-    },{
+    }, {
         id: 21,
         name: "王胜南",
         moneyfrom: 0,
-        type: 3,        //0:村里 1:唐县初中  2:石家庄铁路  3:保定软件学院 4: 倍乐维
-        status: 1,      // 0已送 1未送
+        type: 3, 
+        status: 1, 
         money: 500
-    },{
+    }, {
         id: 22,
         name: "姚顺",
         moneyfrom: 0,
-        type: 2,        //0:村里 1:唐县初中  2:石家庄铁路  3:保定软件学院 4: 倍乐维
-        status: 1,      // 0已送 1未送
+        type: 2, 
+        status: 1, 
         money: 500
-    },{
+    }, {
         id: 23,
         name: "泽洋",
         moneyfrom: 0,
-        type: 0,        //0:村里 1:唐县初中  2:石家庄铁路  3:保定软件学院 4: 倍乐维
-        status: 0,      // 0已送 1未送
+        type: 0, 
+        status: 0, 
         money: 200
-    },{
+    }, {
         id: 24,
         name: "李琪",
         moneyfrom: 0,
-        type: 3,        //0:村里 1:唐县初中  2:石家庄铁路  3:保定软件学院 4: 倍乐维
-        status: 1,      // 0已送 1未送
+        type: 3, 
+        status: 1, 
         money: 500
-    },{
+    }, {
         id: 25,
         name: "姜杨",
         moneyfrom: 0,
-        type: 3,        //0:村里 1:唐县初中  2:石家庄铁路  3:保定软件学院 4: 倍乐维
-        status: 1,      // 0已送 1未送
+        type: 3, 
+        status: 1, 
         money: 500
-    },{
+    }, {
         id: 26,
         name: "马瑞博",
         moneyfrom: 0,
-        type: 0,        //0:村里 1:唐县初中  2:石家庄铁路  3:保定软件学院 4: 倍乐维
-        status: 1,      // 0已送 1未送
+        type: 0, 
+        status: 1, 
         money: 200
-    },{
+    }, {
         id: 27,
         name: "于西",
         moneyfrom: 0,
-        type: 0,        //0:村里 1:唐县初中  2:石家庄铁路  3:保定软件学院 4: 倍乐维
-        status: 0,      // 0已送 1未送
+        type: 0, 
+        status: 0, 
         money: 200
-    },{
+    }, {
         id: 28,
         name: "马兰杰",
         moneyfrom: 0,
-        type: 0,        //0:村里 1:唐县初中  2:石家庄铁路  3:保定软件学院 4: 倍乐维
-        status: 0,      // 0已送 1未送
+        type: 0, 
+        status: 0, 
         money: 200
-    },{
+    }, {
         id: 29,
         name: "耿少京",
         moneyfrom: 0,
-        type: 3,        //0:村里 1:唐县初中  2:石家庄铁路  3:保定软件学院 4: 倍乐维
-        status: 1,      // 0已送 1未送
-        money:500
-    },{
+        type: 3, 
+        status: 1, 
+        money: 500
+    }, {
         id: 30,
         name: "李海峰",
         moneyfrom: 0,
-        type: 3,        //0:村里 1:唐县初中  2:石家庄铁路  3:保定软件学院 4: 倍乐维
-        status: 1,      // 0已送 1未送
+        type: 3, 
+        status: 1, 
         money: 500
-    },{
+    }, {
         id: 31,
         name: "苏怀玉",
         moneyfrom: 0,
-        type: 3,        //0:村里 1:唐县初中  2:石家庄铁路  3:保定软件学院 4: 倍乐维
-        status: 1,      // 0已送 1未送
+        type: 3, 
+        status: 1, 
         money: 500
-    },{
+    }, {
         id: 32,
         name: "罗耀钧",
         moneyfrom: 0,
-        type: 2,        //0:村里 1:唐县初中  2:石家庄铁路  3:保定软件学院 4: 倍乐维
-        status: 1,      // 0已送 1未送
+        type: 2, 
+        status: 1, 
         money: 500
-    },{
+    }, {
         id: 33,
         name: "马云",
         moneyfrom: 0,
-        type: 0,        //0:村里 1:唐县初中  2:石家庄铁路  3:保定软件学院 4: 倍乐维
-        status: 0,      // 0已送 1未送
+        type: 0, 
+        status: 0, 
         money: 200
-    },{
+    }, {
         id: 34,
         name: "袁林华",
         moneyfrom: 0,
-        type: 0,        //0:村里 1:唐县初中  2:石家庄铁路  3:保定软件学院 4: 倍乐维
-        status: 0,      // 0已送 1未送
+        type: 0, 
+        status: 0, 
         money: 200
-    },{
+    }, {
         id: 35,
         name: "硕阳",
         moneyfrom: 0,
-        type: 0,        //0:村里 1:唐县初中  2:石家庄铁路  3:保定软件学院 4: 倍乐维
-        status: 0,      // 0已送 1未送
+        type: 0, 
+        status: 0, 
         money: 200
-    },{
+    }, {
         id: 36,
         name: "于曼",
         moneyfrom: 0,
-        type: 0,        //0:村里 1:唐县初中  2:石家庄铁路  3:保定软件学院 4: 倍乐维
-        status: 0,      // 0已送 1未送
+        type: 0, 
+        status: 0, 
         money: 200
-    },{
+    }, {
         id: 36,
         name: "胡延洲",
         moneyfrom: 0,
-        type: 2,        //0:村里 1:唐县初中  2:石家庄铁路  3:保定软件学院 4: 倍乐维
-        status: 0,      // 0已送 1未送
+        type: 2, 
+        status: 0, 
         money: 500
-    },{
+    }, {
         id: 37,
         name: "王晓强",
         moneyfrom: 0,
-        type: 3,        //0:村里 1:唐县初中  2:石家庄铁路  3:保定软件学院 4: 倍乐维
-        status: 1,      // 0已送 1未送
+        type: 3, 
+        status: 1, 
         money: 500
-    },{
+    }, {
         id: 38,
         name: "马天宇",
         moneyfrom: 2,
-        type: 1,        //0:村里 1:唐县初中  2:石家庄铁路  3:保定软件学院 4: 倍乐维
-        status: 1,      // 0已送 1未送
+        type: 1, 
+        status: 1, 
         money: 500
-    },{
+    }, {
         id: 39,
         name: "赵星明",
         moneyfrom: 1,
-        type: 4,        //0:村里 1:唐县初中  2:石家庄铁路  3:保定软件学院 4: 倍乐维
-        status: 1,      // 0已送 1未送
+        type: 4, 
+        status: 1, 
         money: 800
-    },{
+    }, {
         id: 40,
         name: "于凡",
         moneyfrom: 1,
-        type: 0,        //0:村里 1:唐县初中  2:石家庄铁路  3:保定软件学院 4: 倍乐维
-        status: 0,      // 0已送 1未送
+        type: 0, 
+        status: 0, 
         money: 500
-    },{
+    }, {
         id: 41,
         name: "和浩",
         moneyfrom: 3,
-        type: 1,        //0:村里 1:唐县初中  2:石家庄铁路  3:保定软件学院 4: 倍乐维
-        status: 1,      // 0已送 1未送
+        type: 1, 
+        status: 1, 
         money: 200
-    },{
+    }, {
         id: 42,
         name: "董自强",
         moneyfrom: 3,
-        type: 1,        //0:村里 1:唐县初中  2:石家庄铁路  3:保定软件学院 4: 倍乐维
-        status: 1,      // 0已送 1未送
+        type: 1, 
+        status: 1, 
         money: 200
-    },{
+    }, {
         id: 43,
         name: "马子豪",
         moneyfrom: 3,
-        type: 1,        //0:村里 1:唐县初中  2:石家庄铁路  3:保定软件学院 4: 倍乐维
-        status: 1,      // 0已送 1未送
+        type: 1, 
+        status: 1, 
         money: 200
-    },{
+    }, {
         id: 44,
         name: "张帆",
         moneyfrom: 3,
-        type: 1,        //0:村里 1:唐县初中  2:石家庄铁路  3:保定软件学院 4: 倍乐维
-        status: 1,      // 0已送 1未送
+        type: 1, 
+        status: 1, 
         money: 200
-    },{
+    }, {
         id: 45,
         name: "尤少杰",
         moneyfrom: 3,
-        type: 1,        //0:村里 1:唐县初中  2:石家庄铁路  3:保定软件学院 4: 倍乐维
-        status: 1,      // 0已送 1未送
+        type: 1, 
+        status: 1, 
         money: 200
-    },{
+    }, {
         id: 46,
         name: "王佳豪",
         moneyfrom: 3,
-        type: 1,        //0:村里 1:唐县初中  2:石家庄铁路  3:保定软件学院 4: 倍乐维
-        status: 1,      // 0已送 1未送
+        type: 1, 
+        status: 1, 
         money: 200
-    },{
+    }, {
         id: 47,
         name: "马笑",
         moneyfrom: 3,
-        type: 0,        //0:村里 1:唐县初中  2:石家庄铁路  3:保定软件学院 4: 倍乐维
-        status: 0,      // 0已送 1未送
+        type: 0, 
+        status: 0, 
         money: 500
-    },{
+    }, {
         id: 48,
         name: "于孟飞",
         moneyfrom: 3,
-        type: 0,        //0:村里 1:唐县初中  2:石家庄铁路  3:保定软件学院 4: 倍乐维
-        status: 0,      // 0已送 1未送
+        type: 0, 
+        status: 0, 
         money: 500
-    },{
+    }, {
         id: 49,
         name: "于海洋",
         moneyfrom: 3,
-        type: 0,        //0:村里 1:唐县初中  2:石家庄铁路  3:保定软件学院 4: 倍乐维
-        status: 1,      // 0已送 1未送
+        type: 0, 
+        status: 1, 
         money: 200
-    },{
+    }, {
         id: 50,
         name: "赵森虎",
         moneyfrom: 3,
-        type: 0,        //0:村里 1:唐县初中  2:石家庄铁路  3:保定软件学院 4: 倍乐维
-        status: 1,      // 0已送 1未送
+        type: 0, 
+        status: 1, 
         money: 200
-    },{
+    }, {
         id: 51,
         name: "李达",
         moneyfrom: 3,
-        type: 0,        //0:村里 1:唐县初中  2:石家庄铁路  3:保定软件学院 4: 倍乐维
-        status: 1,      // 0已送 1未送
+        type: 0, 
+        status: 1, 
         money: 200
-    },{
+    }, {
         id: 52,
         name: "刘天华",
         moneyfrom: 3,
-        type: 1,        //0:村里 1:唐县初中  2:石家庄铁路  3:保定软件学院 4: 倍乐维
-        status: 1,      // 0已送 1未送
+        type: 1, 
+        status: 1, 
         money: 200
-    },{
+    }, {
         id: 53,
         name: "魏爽",
         moneyfrom: 3,
-        type: 2,        //0:村里 1:唐县初中  2:石家庄铁路  3:保定软件学院 4: 倍乐维
-        status: 1,      // 0已送 1未送
+        type: 2, 
+        status: 1, 
         money: 500
-    },{
+    }, {
         id: 54,
         name: "马雪风",
         moneyfrom: 3,
-        type: 0,        //0:村里 1:唐县初中  2:石家庄铁路  3:保定软件学院 4: 倍乐维
-        status: 1,      // 0已送 1未送
+        type: 0, 
+        status: 1, 
         money: 200
-    },{
+    }, {
         id: 55,
         name: "康晋博",
         moneyfrom: 0,
-        type: 2,        //0:村里 1:唐县初中  2:石家庄铁路  3:保定软件学院 4: 倍乐维
-        status: 1,      // 0已送 1未送
+        type: 2, 
+        status: 1, 
         money: 500
-    },{
+    }, {
         id: 56,
         name: "韩发",
         moneyfrom: 0,
-        type: 2,        //0:村里 1:唐县初中  2:石家庄铁路  3:保定软件学院 4: 倍乐维
-        status: 1,      // 0已送 1未送
+        type: 2, 
+        status: 1, 
         money: 500
-    }
-    ,{
+    }, {
         id: 57,
         name: "赵玉静",
         moneyfrom: 1,
-        type: 0,        //0:村里 1:唐县初中  2:石家庄铁路  3:保定软件学院 4: 倍乐维
-        status: 1,      // 0已送 1未送
+        type: 0, 
+        status: 1, 
         money: 2000
+    }, {
+        id: 58,
+        name: "郑宝玉",
+        moneyfrom: 0,
+        type: 3, 
+        status: 1, 
+        money: 500
     }
 ]
